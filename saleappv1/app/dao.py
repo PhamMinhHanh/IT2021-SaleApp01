c7666eaeff01744dbc8d08e68988f2808a8031d1
2151053015hanh@ou.edu.vn <2151053015hanh@ou.edu.vn>
2023-10-25 15:05:08 +0700

    init project

---
+++ saleappv1/app/dao.py
@@ -0,0 +1,50 @@
+def load_categories():
+    return [{
+        'id': 1,
+        'name': 'Mobile'
+    },
+        {
+            'id': 2,
+            'name': 'Tablet'
+        }]
+
+
+def load_products():
+    products = [{
+        'id': 1,
+        'name': 'iPhone 13',
+        'price': 13000000,
+        'image': 'https://hc.com.vn/i/ecommerce/media/GS.008504_FEATURE_93851.jpg',
+    },
+        {
+            'id': 2,
+            'name': 'iPhone 13',
+            'price': 13000000,
+            'image': 'https://hc.com.vn/i/ecommerce/media/GS.008504_FEATURE_93851.jpg',
+        },
+        {
+            'id': 4,
+            'name': 'Galaxy S23',
+            'price': 13000000,
+            'image': 'https://hc.com.vn/i/ecommerce/media/GS.008504_FEATURE_93851.jpg',
+        },
+        {
+            'id': 5,
+            'name': 'iPhone 13',
+            'price': 13000000,
+            'image': 'https://hc.com.vn/i/ecommerce/media/GS.008504_FEATURE_93851.jpg',
+        },
+        {
+            'id': 6,
+            'name': 'iPhone 13',
+            'price': 13000000,
+            'image': 'https://hc.com.vn/i/ecommerce/media/GS.008504_FEATURE_93851.jpg',
+        },
+        {
+            'id': 2,
+            'name': 'iPhone 13',
+            'price': 13000000,
+            'image': 'https://hc.com.vn/i/ecommerce/media/GS.008504_FEATURE_93851.jpg',
+        }
+    ]
+    return products
---
+++ saleappv1/app/index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import dao
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    cates = dao.load_categories()
+    products = dao.load_products()
+    return render_template('index.html', categeries=cates, products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
